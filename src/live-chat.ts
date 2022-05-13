f71d1bf965c2dd002d056d82750e5db4c9055147
Reona Oshima(totoraj) <totoradesu-contact@yahoo.co.jp>
2022-05-13 13:44:42 +0900

    updated_metadataに対応

--- src/live-chat.ts
+++ src/live-chat.ts
@@ -1,13 +1,14 @@
 import { EventEmitter } from "events"
 import TypedEmitter from "typed-emitter"
-import { ChatItem } from "./types/data"
+import { ChatItem, MetadataItem } from "./types/data"
 import { FetchOptions } from "./types/yt-response"
-import { fetchChat, fetchLivePage } from "./requests"
+import { fetchChat, fetchLivePage, fetchMetadata } from "./requests"
 
 interface LiveChatEvents {
   start: (liveId: string) => void
   end: (reason?: string) => void
   chat: (chatItem: ChatItem) => void
+  metadata: (metadataItem: MetadataItem) => void
   error: (err: Error | unknown) => void
 }
 
@@ -17,11 +18,14 @@ interface LiveChatEvents {
 export class LiveChat extends (EventEmitter as new () => TypedEmitter<LiveChatEvents>) {
   liveId?: string
   #observer?: NodeJS.Timer
+  #metaObserver?: NodeJS.Timer
   #options?: FetchOptions
+  #metaOptions?: FetchOptions
   readonly #interval: number = 1000
+  readonly #metaInterval: number = 5000
   readonly #id: { channelId: string } | { liveId: string }
 
-  constructor(id: { channelId: string } | { liveId: string }, interval = 1000) {
+  constructor(id: { channelId: string } | { liveId: string }, interval = 1000, metaInterval = 5000) {
     super()
     if (!id || (!("channelId" in id) && !("liveId" in id))) {
       throw TypeError("Required channelId or liveId.")
@@ -31,6 +35,7 @@ export class LiveChat extends (EventEmitter as new () => TypedEmitter<LiveChatEv
 
     this.#id = id
     this.#interval = interval
+    this.#metaInterval = metaInterval
   }
 
   async start(): Promise<boolean> {
@@ -41,8 +46,15 @@ export class LiveChat extends (EventEmitter as new () => TypedEmitter<LiveChatEv
       const options = await fetchLivePage(this.#id)
       this.liveId = options.liveId
       this.#options = options
+      this.#metaOptions = {
+        apiKey: options.apiKey,
+        clientVersion: options.clientVersion,
+        continuation: ""
+      }
 
       this.#observer = setInterval(() => this.#execute(), this.#interval)
+
+      this.#metaObserver = setInterval(() => this.#executeMeta(), this.#metaInterval)
 
       this.emit("start", this.liveId)
       return true
@@ -75,6 +87,22 @@ export class LiveChat extends (EventEmitter as new () => TypedEmitter<LiveChatEv
       this.#options.continuation = continuation
     } catch (err) {
       this.emit("error", err)
+    }
+  }
+
+  async #executeMeta() {
+    if (!this.#metaOptions || !this.liveId) {
+      const message = "Not found options"
+      this.emit("error", new Error(message))
+      this.stop(message)
+      return
+    }
+
+    try {
+      const metadataItem = await fetchMetadata(this.#metaOptions, this.liveId)
+      this.emit("metadata", metadataItem);
+    } catch (err) {
+      this.emit("error", err);
     }
   }
 }
